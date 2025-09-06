955e50fd64905b76b008c3dad4674d79b255d277
PrayasCoder <prayasnasa25@gmail.com>
2025-09-06 19:27:09 +0530

    HandTrackingModule Completed

---
+++ HandTrackingProject/MyNewGameHandTracking.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+pTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList)  != 0:
+        print(lmList[4])
+
+    # FPS calculation
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70),
+                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
