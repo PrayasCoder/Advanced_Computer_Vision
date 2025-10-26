3efb636eb51846c05a202ae01c6c1c91529e93e0
PrayasCoder <prayasnasa25@ce.du.ac.in>
2025-10-26 07:49:58 +0530

    AI Trainer Project Completed

--- PoseEstimationProject/PoseModule.py
+++ PoseEstimationProject/PoseModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class poseDetector():
 
@@ -45,10 +46,32 @@ class poseDetector():
         return self.lmList
     
     def findAngle(self, img, p1, p2, p3, draw=True):
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p1][1:]
+        x3, y3 = self.lmList[p1][1:]
 
-        x1, y2 = self.lmlist[p1][1:]
-        x1, y2 = self.lmlist[p1][1:]
-        x1, y2 = self.lmlist[p1][1:]
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2(y1-y2, x1-x2))
+        if angle < 0:
+            angle += 360
+        print(angle)
+
+
+
+        # Draw
+        if draw: 
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (255,0, 255), 2)
+
+        return angle
 
 
 
